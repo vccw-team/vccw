70bf4e30e1b89a2947c6f4def9c5850d85901d66
miya0001 <miya@wpist.me>
2014-04-28 02:04:02 +0900

    add hatamoto and iemoto

--- site-cookbooks/vccw/recipes/default.rb
+++ site-cookbooks/vccw/recipes/default.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # vim: ft=ruby expandtab shiftwidth=2 tabstop=2
 
-packages = %w{gettext subversion}
+packages = %w{gettext subversion rubygems npm}
 
 packages.each do |pkg|
   package pkg do
@@ -20,5 +20,44 @@ end
 
 execute "echo 'alias makepot.php=\"#{node[:vccw][:makepot]}\"' >> #{node[:vccw]['bash_profile']}" do
   not_if "grep 'alias makepot.php' #{node[:vccw][:bash_profile]}"
+end
+
+execute "npm install -g grunt-init grunt-cli" do
+  user "root"
+  group "root"
+end
+
+execute "gem install sass" do
+  user "root"
+  group "root"
+end
+
+directory '/home/vagrant/.grunt-init' do
+  recursive true
+  user "vagrant"
+  group "vagrant"
+end
+
+git "/home/vagrant/.grunt-init/hatamoto" do
+  repository node[:vccw][:hatamoto_repository]
+  reference  "master"
+  user "vagrant"
+  group "vagrant"
+  action :sync
+end
+
+git "/home/vagrant/.grunt-init/iemoto" do
+  repository node[:vccw][:iemoto_repositry]
+  reference  "master"
+  user "vagrant"
+  group "vagrant"
+  action :sync
+end
+
+template  "/home/vagrant/.grunt-init/defaults.json" do
+  source  "defaults.json.erb"
+  owner   "vagrant"
+  group   "vagrant"
+  mode    "0600"
 end
 
