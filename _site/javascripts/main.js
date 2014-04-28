4acf92e8179ab43d51f6c98c22264b06d5140f2f
miya0001 <miya@wpist.me>
2014-04-29 08:02:13 +0900

    some fix

--- _site/javascripts/main.js
+++ _site/javascripts/main.js
@@ -1,19 +1,14 @@
 (function($){
-
-    // anchor link
-    $('#main-content h2').each(function(i){
+  $('#main-content h2').each(function(i){
     $(this).attr('id', 'h2-'+i);
     $('#navmenu').append('<li><a href="#h2-'+i+'">'+$(this).text()+'</a></li>');
-    });
-
-    // smooth scroll
-    $('a[href^=#]').click(function(){
+  });
+  $('a[href^=#]').click(function(){
     var speed = 500;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
-    });
-
+  });
 })(jQuery);
