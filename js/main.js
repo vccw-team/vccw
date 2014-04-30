0fc0b64fce9cf0be1480f0c13beffd4cdaf1582a
miya0001 <miya@wpist.me>
2014-04-30 15:30:52 +0900

    fix js problem

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 (function(){
 
     // anchor link
-    $('#main-content h2').each(function(i){
-    $(this).attr('id', 'h2-'+i);
-    $('#navmenu').append('<li><a href="#h2-'+i+'">'+$(this).text()+'</a></li>');
-    });
+    if ($('#navmenu').len()) {
+        $('#main-content h2').each(function(i){
+            $(this).attr('id', 'h2-'+i);
+            $('#navmenu').append('<li><a href="#h2-'+i+'">'+$(this).text()+'</a></li>');
+        });
+    }
 
     // smooth scroll
     $('a[href^=#]').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
     });
 
 })();
