8afa4876055c5016b898b627432f28836a7ef3ba
miya0001 <miya@wpist.me>
2014-05-02 04:26:30 +0900

    oops

--- cookbooks/dmg/metadata.rb
+++
@@ -1,9 +0,0 @@
-# Encoding: utf-8
-name             'dmg'
-maintainer       'Opscode, Inc'
-maintainer_email 'cookbooks@opscode.com'
-license          'Apache 2.0'
-description      'LWRP to install OS X applications from dmgs'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.0'
-supports         'mac_os_x'
