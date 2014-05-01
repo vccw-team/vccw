c4fda580321cbb963b3ab770ca5b28ac2edbe74b
miya0001 <miya@wpist.me>
2014-05-02 01:33:32 +0900

    Import yum-epel version 0.3.6

---
+++ cookbooks/yum-epel/attributes/epel-testing-source.rb
@@ -0,0 +1,24 @@
+default['yum']['epel-testing-source']['repositoryid'] = 'epel-testing-source'
+
+case node['platform']
+when 'amazon'
+  default['yum']['epel-testing-source']['description'] = 'Extra Packages for Enterprise Linux 6 - $basearch'
+  default['yum']['epel-testing-source']['mirrorlist'] = 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  default['yum']['epel-testing-source']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+else
+  case node['platform_version'].to_i
+  when 5
+    default['yum']['epel-testing-source']['description'] = 'Extra Packages for Enterprise Linux 5 - Testing - $basearch Source'
+    default['yum']['epel-testing-source']['mirrorlist'] = 'http://mirrors.fedoraproject.org/mirrorlist?repo=testing-source-epel5&arch=$basearch'
+    default['yum']['epel-testing-source']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
+  when 6
+    default['yum']['epel-testing-source']['description'] = 'Extra Packages for Enterprise Linux 6 - Testing - $basearch Source'
+    default['yum']['epel-testing-source']['mirrorlist'] = 'https://mirrors.fedoraproject.org/metalink?repo=testing-source-epel6&arch=$basearch'
+    default['yum']['epel-testing-source']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+  end
+end
+
+default['yum']['epel-testing-source']['failovermethod'] = 'priority'
+default['yum']['epel-testing-source']['gpgcheck'] = true
+default['yum']['epel-testing-source']['enabled'] = false
+default['yum']['epel-testing-source']['managed'] = false
