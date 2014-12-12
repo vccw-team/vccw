2bd2c97b5146cab409edc883fdb6d4b626caa071
miya0001 <miya@wpist.me>
2014-12-13 05:03:54 +0900

    rename and update test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,8 +25,7 @@ RSpec.configure do |c|
       c.host  = host
       options = Net::SSH::Config.for(c.host)
       user    = options[:user] || Etc.getlogin
-      # vagrant_up = `VAGRANT_VAGRANTFILE=Vagrantfile.sample vagrant up default`
-      config = `VAGRANT_VAGRANTFILE=Vagrantfile.sample vagrant ssh-config default`
+      config = `vagrant ssh-config default`
       if config != ''
         config.each_line do |line|
           if match = /HostName (.*)/.match(line)
