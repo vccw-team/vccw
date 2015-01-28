1686cdbf29c890f3da27fba152eefa8e3dc4d85d
miya0001 <miya@wpist.me>
2015-01-28 22:25:27 +0900

    some fix

--- provision-post.sh
+++ provision-post.sh
@@ -8,6 +8,7 @@ sudo rm -fr /var/www/wordpress
 sudo rm -f /etc/httpd/sites-available/wordpress.conf
 sudo rm -f /etc/httpd/sites-enabled/wordpress.conf
 
+rm -f /etc/ssh/ssh_host_*
 cd /var/log
 find /var/log/ -type f -name '*.log' -exec cp /dev/null {} \;
 cp /dev/null /var/log/syslog
@@ -18,6 +19,11 @@ yes | cp /dev/null /home/vagrant/.bash_history
 sudo /etc/init.d/vboxadd setup
 
 sudo ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
+
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
+
+chmod 700 /home/vagrant/.ssh
+chmod 600 /home/vagrant/.ssh/authorized_keys
 
 history -c
 
