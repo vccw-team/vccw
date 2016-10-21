5982c0e8bf2cb47949d703bf581cb0d69ae5816f
miya0001 <miya@wpist.me>
2016-10-21 20:09:08 +0900

    fix #169

--- spec/default/php_spec.rb
+++ spec/default/php_spec.rb
@@ -1,28 +1,15 @@
 require 'spec_helper'
 
 describe 'PHP config parameters' do
-  context php_config('default_charset', :ini => '/etc/php/7.0/apache2/php.ini') do
-    its(:value) { should eq 'UTF-8' }
-  end
-
-  context  php_config('mbstring.language', :ini => '/etc/php/7.0/apache2/php.ini') do
-    its(:value) { should eq 'neutral' }
-  end
-
-  context  php_config('mbstring.internal_encoding', :ini => '/etc/php/7.0/apache2/php.ini') do
-    its(:value) { should eq 'UTF-8' }
-  end
-
-  context php_config('date.timezone', :ini => '/etc/php/7.0/apache2/php.ini') do
-    its(:value) { should eq 'UTC' }
-  end
-
-  context php_config('short_open_tag', :ini => '/etc/php/7.0/apache2/php.ini') do
-    its(:value) { should_not eq 1 }
-    its(:value) { should_not eq 'On' }
-  end
-
-  context php_config('session.save_path', :ini => '/etc/php/7.0/apache2/php.ini') do
-    its(:value) { should eq '/tmp' }
+  $conf['php_ini'].each do |ini_key, ini_value|
+    context php_config(ini_key, :ini => '/etc/php/7.0/apache2/conf.d/99-vccw.ini') do
+      if true == ini_value
+        its(:value) { should eq 1 }
+      elsif false == ini_value
+        its(:value) { should eq "" }
+      else
+        its(:value) { should eq ini_value }
+      end
+    end
   end
 end
