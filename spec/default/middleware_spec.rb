0af5629dcb6162d62519ffa5ff67674001f3db0a
miya0001 <miya@wpist.me>
2016-10-23 02:35:36 +0900

    move php tests

--- spec/default/middleware_spec.rb
+++ spec/default/middleware_spec.rb
@@ -97,3 +97,17 @@ commands.each do |commands|
     its(:exit_status) { should eq 0 }
   end
 end
+
+describe 'PHP config parameters' do
+  $conf['php_ini'].each do |ini_key, ini_value|
+    context php_config(ini_key, :ini => '/etc/php/7.0/apache2/conf.d/99-vccw.ini') do
+      if true == ini_value
+        its(:value) { should eq 1 }
+      elsif false == ini_value
+        its(:value) { should eq "" }
+      else
+        its(:value) { should eq ini_value }
+      end
+    end
+  end
+end
