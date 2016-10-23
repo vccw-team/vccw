90cba96e36bc935a56a2659c98f28514c0043de4
miya0001 <miya@wpist.me>
2016-10-23 19:01:04 +0900

    enables to add php code into wp-config.php

--- provision/playbooks/templates/extra-wp-config.php
+++ provision/playbooks/templates/extra-wp-config.php
@@ -2,3 +2,5 @@ define( 'JETPACK_DEV_DEBUG', {{ vccw.wp_debug }} );
 define( 'WP_DEBUG', {{ vccw.wp_debug }} );
 define( 'FORCE_SSL_ADMIN', {{ vccw.force_ssl_admin }} );
 define( 'SAVEQUERIES', {{ vccw.savequeries }} );
+
+{{ vccw.extra_wp_config }}
