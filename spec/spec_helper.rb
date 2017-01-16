dbfc908897dd650b63c4ae430e3132cab7f3260b
miya0001 <miya@wpist.me>
2017-01-16 17:23:44 +0900

    allows configure from env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,18 @@ $conf = YAML.load(
   ).read
 )
 
-if ENV['ANSIBLE_ENV'].instance_of?( String )
-  site = JSON.parse( ENV['ANSIBLE_ENV'] );
+if File.exists?( 'site.yml' )
+  site = YAML.load(
+    File.open(
+      'site.yml',
+      File::RDONLY
+    ).read
+  )
+  $conf.merge!( site ) if site.is_a?( Hash )
+end
+
+if ENV['VCCW_ENV'].instance_of?( String )
+  site = JSON.parse( ENV['VCCW_ENV'] );
   $conf.merge!( site ) if site.is_a?( Hash );
 end
 
