e01e6f14eb452a8ead7697775700bad80201dd94
miya0001 <miya@wpist.me>
2017-02-05 07:25:26 +0900

    add shell script for automatic deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eux
+
+rm -fr .github
+rm -fr bin
+rm -fr spec
+rm -f .envrc
+rm -f .gitattribute
+rm -f .rspec
+rm -f .travis.yml
+rm -f Gemfile
+rm -f Gemfile.lock
+rm -f Rakefile
+
+cat <<EOF > .gitignore
+.DS_Store
+.ansible
+.bundle
+.envrc
+.github/
+.idea/
+.travis.yml
+.vagrant
+npm-debug.log
+package.box
+playbook.retry
+vendor
+EOF
+
+zip -r vccw.zip .
