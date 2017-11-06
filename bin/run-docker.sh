604f7bc968fe7c360ac6e33f0801ae89a57353a4
miya0001 <miya@wpist.me>
2017-11-07 04:56:19 +0900

    run playbook user 1000

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -42,7 +42,5 @@ docker run -idt -p 80:80 -p 443:443 \
 vccw/vccw-xenial64:latest \
 "/sbin/init"
 
-docker exec --tty ${VCCW_HOSTNAME} env TERM=xterm chown ubuntu:ubuntu ${VM_DIR}
-
-docker exec --user ubuntu --tty ${VCCW_HOSTNAME} \
+docker exec --user 1000 --tty ${VCCW_HOSTNAME} \
 env TERM=xterm ansible-playbook ${VM_DIR}/provision/playbook.yml -e "$(ruby $RUBY)"
