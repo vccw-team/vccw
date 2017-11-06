a66bd7ed6a05d43fa351b0d059833b90906d1a0a
miya0001 <miya@wpist.me>
2017-11-07 03:48:32 +0900

    chown docker dir

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -42,8 +42,7 @@ docker run -idt -p 80:80 -p 443:443 \
 vccw/vccw-xenial64:latest \
 "/sbin/init"
 
-docker exec --tty ${VCCW_HOSTNAME} env TERM=xterm touch ${VM_DIR}/wp-cli.yml
-docker exec --tty ${VCCW_HOSTNAME} env TERM=xterm chmod 777 ${VM_DIR}/wp-cli.yml
+docker exec --tty ${VCCW_HOSTNAME} env TERM=xterm chown ubuntu:ubuntu ${VM_DIR}
 
 docker exec --user ubuntu --tty ${VCCW_HOSTNAME} \
 env TERM=xterm ansible-playbook ${VM_DIR}/provision/playbook.yml -e "$(ruby $RUBY)"
