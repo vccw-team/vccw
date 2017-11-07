e30f0df59ecdefe254a2157e15830b498bd81d70
miya0001 <miya@wpist.me>
2017-11-08 05:26:55 +0900

    add browse command

---
+++ provision/playbooks/templates/browse-command.php
@@ -0,0 +1,33 @@
+<?php
+
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
+/**
+ * Open the website in your browser.
+ *
+ * ## Options
+ *
+ * [--wp-admin]
+ * : Open the dashboard.
+ *
+ */
+$browse_command = function( $_, $assoc_args ) {
+	$exec = 'xdg-open';
+	if ( 'DAR' === strtoupper( substr( PHP_OS, 0, 3 ) ) ) {
+		$exec = 'open';
+	}
+
+	$wp_admin = \WP_CLI\Utils\get_flag_value( $assoc_args, 'wp-admin' );
+
+	if ( true === $wp_admin ) {
+		$url = 'http://{{ vccw.hostname }}/wp-admin/';
+	} else {
+		$url = 'http://{{ vccw.hostname }}/';
+	}
+
+	passthru( $exec . ' ' . escapeshellarg( $url ) );
+};
+
+WP_CLI::add_command( 'browse', $browse_command );
