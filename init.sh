d4528abbcc9f5f32049fecc09991fb7d2da76f6d
Anders Aarvik <aarvik92@gmail.com>
2017-12-17 17:29:22 +0100

    Added first version of init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
 cp ./provision/default.yml ./site.yml
+printf "Domain name (eg. promocode.co.ke): "; read domain
+sed -i -e "/hostname:/s/.*/hostname: $domain/" site.yml
 
+echo -n "Do you want to work on an existing Wordpress website (y/n)?"
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    echo -e "\nInstallation steps: "
+    echo "1) Run '$ vagrant up' and make sure to provision."
+    echo "2) Replace the files from the existing Wordpress application with the files in ./wordpress/, except ./wordpress/phpmyadmin directory."
+    echo "3) Login to phpMyAdmin at http://$domain/phpmyadmin, on database 'wordpress', with username 'wordpress' and password 'wordpress', and import your existing database. Make sure your wp-config.php matches these database credentials."
+    echo "4) Enjoy developing with your files at ./wordpress/ shared with vm folder /var/www/html, with local access at http://$domain."
+else
+    printf "Website title: "; read title
+    sed -i -e "/title:/s/.*/title: $title/" site.yml
+    
+    printf "WordPress version (eg. latest): "; read wp_version
+    sed -i -e "/version:/s/.*/version: $wp_version/" site.yml
+    
+    printf "Wordpress username (eg. admin): "; read wp_username
+    sed -i -e "/admin_user:/s/.*/admin_user: $wp_username/" site.yml
+    
+    printf "Wordpress password (eg. admin): "; read wp_password
+    sed -i -e "/admin_pass:/s/.*/admin_pass: $admin_pass/" site.yml
 
+    echo -e "\nInstallation Steps:"
+    echo "1) Run '$ vagrant up' and make sure to provision."
+    echo "2) Enjoy developing with your files at ./wordpress/ shared with vm folder /var/www/html, with local access at http://$domain."
+    echo "3) You can manage the database from phpMyAdmin at http://$domain/phpmyadmin with database 'wordpress', with username 'wordpress' and password 'wordpress'."
+fi
