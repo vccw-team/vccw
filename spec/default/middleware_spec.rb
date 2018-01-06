942dcdc92abcff5b25e382a412e7d36095ba8b91
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-01-06 17:48:00 +0900

    remove test for mysql enabled (#304)

--- spec/default/middleware_spec.rb
+++ spec/default/middleware_spec.rb
@@ -33,11 +33,6 @@ describe package('mysql-server') do
   it { should be_installed }
 end
 
-describe service('mysql') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe port(3306) do
   it { should be_listening }
 end
@@ -125,4 +120,3 @@ describe 'PHP config parameters for cli' do
     end
   end
 end
-
